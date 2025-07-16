c9d8e7a27333fbc3e5ffc056d2adfbdd77f05acc
Danny Nguyen <ndanny09@gmail.com>
2025-07-16 12:31:24 -0400

    1.0.1: Define GUI's

---
+++ src/main/java/me/bam6561/aethelcomposite/guis/GUI.java
@@ -0,0 +1,57 @@
+package me.bam6561.aethelcomposite.guis;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * {@link me.bam6561.aethelcomposite.Plugin} managed inventory.
+ *
+ * @author Danny Nguyen
+ * @version 1.0.1
+ * @since 1.0.1
+ */
+public abstract class GUI implements InventoryHandler {
+  /**
+   * Inventory.
+   */
+  private final Inventory inventory = createInventory();
+
+  /**
+   * No parameter constructor.
+   */
+  public GUI() {
+  }
+
+  /**
+   * Creates the inventory.
+   *
+   * @return inventory
+   */
+  @NotNull
+  protected abstract Inventory createInventory();
+
+  /**
+   * Adds inventory buttons.
+   */
+  protected abstract void addButtons();
+
+  /**
+   * What to do when a click occurs outside the inventory
+   * view and clicks inside the player's inventory.
+   *
+   * @param event inventory click event
+   * @return finished interaction
+   */
+  protected abstract boolean isNullOrPlayerInventoryClick(@NotNull InventoryClickEvent event);
+
+  /**
+   * Gets the inventory.
+   *
+   * @return inventory
+   */
+  @NotNull
+  public Inventory getInventory() {
+    return this.inventory;
+  }
+}
---
+++ src/main/java/me/bam6561/aethelcomposite/guis/InventoryHandler.java
@@ -0,0 +1,44 @@
+package me.bam6561.aethelcomposite.guis;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Outlines base interactions for a plugin managed inventory.
+ *
+ * @author Danny Nguyen
+ * @version 1.0.1
+ * @since 1.0.1
+ */
+public interface InventoryHandler {
+  /**
+   * What to do when a click occurs.
+   *
+   * @param event inventory click event
+   */
+  void onClick(@NotNull InventoryClickEvent event);
+
+  /**
+   * What to do when a drag click occurs.
+   *
+   * @param event inventory drag event
+   */
+  void onDrag(@NotNull InventoryDragEvent event);
+
+  /**
+   * What to do when the inventory is opened.
+   *
+   * @param event inventory open event
+   */
+  void onOpen(@NotNull InventoryOpenEvent event);
+
+  /**
+   * What to do when the inventory is closed.
+   *
+   * @param event inventory close event
+   */
+  void onClose(@NotNull InventoryCloseEvent event);
+}
