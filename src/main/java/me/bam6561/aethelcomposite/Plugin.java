933928a7e47975dbeddbc3ff8a048ddf8aae42a2
Danny Nguyen <ndanny09@gmail.com>
2025-07-16 10:36:09 -0400

    0: Base Plugin Setup

---
+++ src/main/java/me/bam6561/aethelcomposite/Plugin.java
@@ -0,0 +1,43 @@
+package me.bam6561.aethelcomposite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Represents the plugin as an object.
+ * <p>
+ * Through event listeners and command executors, the plugin can
+ * handle various requests given to it by its users and the server.
+ * </p>
+ *
+ * @author Danny Nguyen
+ * @version 0
+ * @since 0
+ */
+
+public class Plugin extends JavaPlugin {
+  /**
+   * No parameter constructor.
+   */
+  public Plugin() {
+  }
+
+  /**
+   * On enable:
+   * <ul>
+   *   <li>
+   * </ul>
+   */
+  @Override
+  public void onEnable() {
+  }
+
+  /**
+   * On disable:
+   * <ul>
+   *   <li>
+   * </ul>
+   */
+  @Override
+  public void onDisable() {
+  }
+}
