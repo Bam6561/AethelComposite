f5109ac5a18285d46ed6e2007908461287c26302
Danny Nguyen <ndanny09@gmail.com>
2025-07-18 10:22:57 -0400

    1.0.26: Define Module.List

--- src/main/java/me/bam6561/aethelcomposite/references/Module.java
+++ src/main/java/me/bam6561/aethelcomposite/references/Module.java
@@ -8,8 +8,6 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
 /**
  * {@link Plugin} modules.
  * <p>
@@ -25,6 +23,24 @@ public class Module {
    */
   private Module() {
   }
+
+  /**
+   * List of modules.
+   *
+   * @author Danny Nguyen
+   * @version 1.0.26
+   * @since 1.0.26
+   */
+  public enum List {
+    LASSO;
+
+    /**
+     * No parameter constructor.
+     */
+    List() {
+    }
+  }
+
 
   /**
    * Lasso module.
