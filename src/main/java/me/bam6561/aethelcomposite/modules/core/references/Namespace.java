5d33efea7c700ba9007279b7748bfc9057e1ebf6
Danny Nguyen <ndanny09@gmail.com>
2025-07-18 21:52:05 -0400

    1.0.38: Move Namespace Components to Separate Class

---
+++ src/main/java/me/bam6561/aethelcomposite/modules/core/references/Namespace.java
@@ -0,0 +1,126 @@
+package me.bam6561.aethelcomposite.modules.core.references;
+
+import me.bam6561.aethelcomposite.Plugin;
+import me.bam6561.aethelcomposite.modules.core.references.markers.NamespacedKeyValue;
+import me.bam6561.aethelcomposite.modules.core.references.markers.StringValue;
+import org.bukkit.NamespacedKey;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * {@link Plugin} namespaces.
+ *
+ * @author Danny Nguyen
+ * @version 1.0.38
+ * @since 1.0.38
+ */
+public class Namespace {
+  /**
+   * Enum usage only.
+   */
+  private Namespace() {
+  }
+
+  /**
+   * Reserved namespaced key headers.
+   * <p>
+   * Key headers are divided into categories by period characters for parsing.
+   *
+   * @author Danny Nguyen
+   * @version 1.0.38
+   * @since 1.0.28
+   */
+  public enum Header implements StringValue {
+    /**
+     * {@link Plugin}
+     */
+    AETHEL("aethel."),
+
+    /**
+     * Tags describing an item's meta.
+     */
+    ITEM(AETHEL.asString() + "item.");
+
+    /**
+     * Key header.
+     */
+    private final String header;
+
+    /**
+     * Associates the entry with the key header.
+     *
+     * @param header key header
+     */
+    Header(String header) {
+      this.header = header;
+    }
+
+    /**
+     * Gets the key header.
+     *
+     * @return key header
+     */
+    @Override
+    @NotNull
+    public String asString() {
+      return this.header;
+    }
+  }
+
+  /**
+   * Reserved namespaced keys.
+   *
+   * @author Danny Nguyen
+   * @version 1.0.34
+   * @since 1.0.29
+   */
+  public enum Key implements NamespacedKeyValue, StringValue {
+    /**
+     * Item ID.
+     * <p>
+     * Spaces are replaced with underscores.
+     */
+    ITEM_ID(new NamespacedKey(Plugin.getInstance(), Header.ITEM.asString() + "id"), Header.ITEM.asString() + "id");
+
+    /**
+     * Namespaced key.
+     */
+    private final NamespacedKey key;
+
+    /**
+     * Namespaced key string.
+     */
+    private final String keyString;
+
+    /**
+     * Associates the entry with the namespaced key and namespaced key string.
+     *
+     * @param key       namespaced key
+     * @param keyString namespaced key string
+     */
+    Key(NamespacedKey key, String keyString) {
+      this.key = key;
+      this.keyString = keyString;
+    }
+
+    /**
+     * Gets the namespaced key.
+     *
+     * @return namespaced key
+     */
+    @NotNull
+    public NamespacedKey asKey() {
+      return this.key;
+    }
+
+    /**
+     * Gets the namespaced key string.
+     *
+     * @return namespaced key string
+     */
+    @Override
+    @NotNull
+    public String asString() {
+      return this.keyString;
+    }
+  }
+}
--- src/main/java/me/bam6561/aethelcomposite/modules/core/references/Text.java
+++ src/main/java/me/bam6561/aethelcomposite/modules/core/references/Text.java
@@ -2,17 +2,15 @@ package me.bam6561.aethelcomposite.modules.core.references;
 
 import me.bam6561.aethelcomposite.Plugin;
 import me.bam6561.aethelcomposite.modules.core.references.markers.ChatColorValue;
-import me.bam6561.aethelcomposite.modules.core.references.markers.NamespacedKeyValue;
 import me.bam6561.aethelcomposite.modules.core.references.markers.StringValue;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.NamespacedKey;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * {@link Plugin} text and internal keys.
+ * {@link Plugin} text.
  *
  * @author Danny Nguyen
- * @version 1.0.18
+ * @version 1.0.38
  * @since 1.0.18
  */
 public class Text {
@@ -78,111 +76,6 @@ public class Text {
     @NotNull
     public String asString() {
       return this.colorName;
-    }
-  }
-
-  /**
-   * Reserved namespaced key headers.
-   * <p>
-   * Key headers are divided into categories by period characters for parsing.
-   *
-   * @author Danny Nguyen
-   * @version 1.0.28
-   * @since 1.0.28
-   */
-  public enum KeyHeader implements StringValue {
-    /**
-     * {@link Plugin}
-     */
-    AETHEL("aethel."),
-
-    /**
-     * Tags describing an item's meta.
-     */
-    ITEM(AETHEL.asString() + "item.");
-
-    /**
-     * Key header.
-     */
-    private final String header;
-
-    /**
-     * Associates the entry with the key header.
-     *
-     * @param header key header
-     */
-    KeyHeader(String header) {
-      this.header = header;
-    }
-
-    /**
-     * Gets the key header.
-     *
-     * @return key header
-     */
-    @Override
-    @NotNull
-    public String asString() {
-      return this.header;
-    }
-  }
-
-
-  /**
-   * Reserved namespaced keys.
-   *
-   * @author Danny Nguyen
-   * @version 1.0.34
-   * @since 1.0.29
-   */
-  public enum Key implements NamespacedKeyValue, StringValue {
-    /**
-     * Item ID.
-     * <p>
-     * Spaces are replaced with underscores.
-     */
-    ITEM_ID(new NamespacedKey(Plugin.getInstance(), KeyHeader.ITEM.asString() + "id"), KeyHeader.ITEM.asString() + "id");
-
-    /**
-     * Namespaced key.
-     */
-    private final NamespacedKey key;
-
-    /**
-     * Namespaced key string.
-     */
-    private final String keyString;
-
-    /**
-     * Associates the entry with the namespaced key and namespaced key string.
-     *
-     * @param key       namespaced key
-     * @param keyString namespaced key string
-     */
-    Key(NamespacedKey key, String keyString) {
-      this.key = key;
-      this.keyString = keyString;
-    }
-
-    /**
-     * Gets the namespaced key.
-     *
-     * @return namespaced key
-     */
-    @NotNull
-    public NamespacedKey asKey() {
-      return this.key;
-    }
-
-    /**
-     * Gets the namespaced key string.
-     *
-     * @return namespaced key string
-     */
-    @Override
-    @NotNull
-    public String asString() {
-      return this.keyString;
     }
   }
 }
