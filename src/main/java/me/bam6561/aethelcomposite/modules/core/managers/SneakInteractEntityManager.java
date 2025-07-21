0ac699a816102dc8b0faad36af8346d67fdc4d8c
Danny Nguyen <ndanny09@gmail.com>
2025-07-21 10:42:31 -0400

    1.0.52: Unfinished PlayerInteractEntity Lasso Interaction

--- src/main/java/me/bam6561/aethelcomposite/modules/core/managers/SneakInteractEntityManager.java
+++ src/main/java/me/bam6561/aethelcomposite/modules/core/managers/SneakInteractEntityManager.java
@@ -1,7 +1,13 @@
 package me.bam6561.aethelcomposite.modules.core.managers;
 
 import me.bam6561.aethelcomposite.modules.core.events.player.SneakInteractEntityEvent;
+import me.bam6561.aethelcomposite.modules.core.references.Namespaced;
+import me.bam6561.aethelcomposite.utils.ItemUtils;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -10,7 +16,7 @@ import java.util.Objects;
  * Manages {@link SneakInteractEntityEvent} interactions.
  *
  * @author Danny Nguyen
- * @version 1.0.22
+ * @version 1.0.52
  * @since 1.0.8
  */
 public class SneakInteractEntityManager {
@@ -30,5 +36,25 @@ public class SneakInteractEntityManager {
    */
   public void interpretAction(@NotNull PlayerInteractEntityEvent event) {
     Objects.requireNonNull(event, "Null event");
+    Player player = event.getPlayer();
+    PlayerInventory inv = player.getInventory();
+
+    ItemStack mainHandItem = inv.getItemInMainHand();
+    if (ItemUtils.Read.isNotNullOrAir(mainHandItem)) {
+      String itemID = mainHandItem.getItemMeta().getPersistentDataContainer().get(Namespaced.Key.ITEM_ID.asKey(), PersistentDataType.STRING);
+      switch (itemID) {
+        case "iron_lasso" -> {
+
+        }
+        case "golden_lasso" -> {
+
+        }
+        case "diamond_lasso" -> {
+
+        }
+        case "emerald_lasso" -> {
+        }
+      }
+    }
   }
 }
