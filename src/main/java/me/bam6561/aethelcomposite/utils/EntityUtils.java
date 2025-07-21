9bc19256e460e47e19218d7822e092f458f2b16c
Danny Nguyen <ndanny09@gmail.com>
2025-07-21 12:26:09 -0400

    1.0.59: Define EntityUtils.Data

--- src/main/java/me/bam6561/aethelcomposite/utils/EntityUtils.java
+++ src/main/java/me/bam6561/aethelcomposite/utils/EntityUtils.java
@@ -1,13 +1,24 @@
 package me.bam6561.aethelcomposite.utils;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Objects;
 
 /**
  * Utilities for Entities.
  *
  * @author Danny Nguyen
- * @version 1.0.57
+ * @version 1.0.59
  * @since 1.0.57
  */
 public class EntityUtils {
@@ -18,7 +29,7 @@ public class EntityUtils {
   }
 
   /**
-   * Spawns Entities.
+   * Spawns entities.
    *
    * @author Danny Nguyen
    * @version 1.0.57
@@ -42,6 +53,58 @@ public class EntityUtils {
     public void spawnEntity(Location location, EntityType entityType, int spawns, boolean isRandomized) {
       for (int i = 0; i < spawns; i++) {
         location.getWorld().spawnEntity(location, entityType, isRandomized);
+      }
+    }
+  }
+
+  /**
+   * Serializes and deserializes entities.
+   *
+   * @author Danny Nguyen
+   * @version 1.0.59
+   * @since 1.0.59
+   */
+  public static class Data {
+    /**
+     * Utility methods only.
+     */
+    private Data() {
+    }
+
+    /**
+     * Serializes an entity into a byte string.
+     *
+     * @param entity entity to encode
+     * @return serialized entity string
+     */
+    @Nullable
+    public static String encodeEntityString(@NotNull Entity entity) {
+      Objects.requireNonNull(entity, "Null entity");
+      try {
+        ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+        BukkitObjectOutputStream bukkitObjectOutput = new BukkitObjectOutputStream(byteOutput);
+        bukkitObjectOutput.writeObject(entity);
+        bukkitObjectOutput.flush();
+        return Base64.getEncoder().encodeToString(byteOutput.toByteArray());
+      } catch (IOException ex) {
+        return null;
+      }
+    }
+
+    /**
+     * Deserializes an entity byte string.
+     *
+     * @param data serialized entity string
+     * @return decoded entity
+     */
+    public static Entity decodeEntityString(@NotNull String data) {
+      Objects.requireNonNull(data, "Null data");
+      try {
+        ByteArrayInputStream byteInput = new ByteArrayInputStream(Base64.getDecoder().decode(data));
+        BukkitObjectInputStream bukkitObjectInput = new BukkitObjectInputStream(byteInput);
+        return (Entity) bukkitObjectInput.readObject();
+      } catch (IOException | ClassNotFoundException ex) {
+        return null;
       }
     }
   }
