88d4ef74b7982a003c9774d0241d24522e099aaa
Danny Nguyen <ndanny09@gmail.com>
2025-07-22 08:06:42 -0400

    1.0.71: Move Logic Into activateItemAbility Method

--- src/main/java/me/bam6561/aethelcomposite/modules/core/managers/SneakInteractEntityManager.java
+++ src/main/java/me/bam6561/aethelcomposite/modules/core/managers/SneakInteractEntityManager.java
@@ -2,13 +2,12 @@ package me.bam6561.aethelcomposite.modules.core.managers;
 
 import me.bam6561.aethelcomposite.Plugin;
 import me.bam6561.aethelcomposite.modules.core.events.player.SneakInteractEntityEvent;
+import me.bam6561.aethelcomposite.modules.core.references.Namespaced;
 import me.bam6561.aethelcomposite.modules.lasso.references.Lasso;
 import me.bam6561.aethelcomposite.utils.ItemUtils;
 import me.bam6561.aethelcomposite.utils.TextUtils;
-import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -17,7 +16,7 @@ import java.util.Objects;
  * Manages {@link SneakInteractEntityEvent} interactions.
  *
  * @author Danny Nguyen
- * @version 1.0.65
+ * @version 1.0.71
  * @since 1.0.8
  */
 public class SneakInteractEntityManager {
@@ -30,17 +29,14 @@ public class SneakInteractEntityManager {
   /**
    * On interaction:
    * <ul>
-   *   <li>
+   *   <li> {@link #activateItemAbility(PlayerInteractEntityEvent, String)}
    * </ul>
    *
    * @param event player interact entity event
    */
   public void interpretAction(@NotNull PlayerInteractEntityEvent event) {
     Objects.requireNonNull(event, "Null event");
-    Player player = event.getPlayer();
-    PlayerInventory inv = player.getInventory();
-    ItemStack mainHandItem = inv.getItemInMainHand();
-
+    ItemStack mainHandItem = event.getPlayer().getInventory().getItemInMainHand();
     if (ItemUtils.Read.isNullOrAir(mainHandItem)) {
       return;
     }
@@ -50,6 +46,16 @@ public class SneakInteractEntityManager {
       return;
     }
 
+    activateItemAbility(event, itemID);
+  }
+
+  /**
+   * Activates an item's ability.
+   *
+   * @param event  player interact entity event
+   * @param itemID {@link Namespaced.Key#ITEM_ID}
+   */
+  private void activateItemAbility(PlayerInteractEntityEvent event, String itemID) {
     switch (itemID) {
       case "iron_lasso", "golden_lasso", "diamond_lasso", "emerald_lasso" ->
           Plugin.getLassoManager().captureEntity(event, Lasso.Item.valueOf(TextUtils.Format.asEnum(itemID)));
