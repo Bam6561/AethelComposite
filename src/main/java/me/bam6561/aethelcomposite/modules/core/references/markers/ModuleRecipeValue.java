5f05b73f7d36ba741cfa6904e52a5861393080c2
Danny Nguyen <ndanny09@gmail.com>
2025-07-23 16:30:46 -0400

    1.0.90: Define ModuleRecipeValue

---
+++ src/main/java/me/bam6561/aethelcomposite/modules/core/references/markers/ModuleRecipeValue.java
@@ -0,0 +1,21 @@
+package me.bam6561.aethelcomposite.modules.core.references.markers;
+
+import me.bam6561.aethelcomposite.modules.core.markers.ModuleRecipe;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * References usable as {@link ModuleRecipe ModuleRecipes}.
+ *
+ * @author Danny Nguyen
+ * @version 1.0.90
+ * @since 1.0.90
+ */
+public interface ModuleRecipeValue {
+  /**
+   * Gets the enum's {@link ModuleRecipe} value.
+   *
+   * @return enum's {@link ModuleRecipe} value
+   */
+  @NotNull
+  ModuleRecipe asModuleRecipe();
+}
--- src/main/java/me/bam6561/aethelcomposite/modules/lasso/references/Lasso.java
+++ src/main/java/me/bam6561/aethelcomposite/modules/lasso/references/Lasso.java
@@ -6,6 +6,7 @@ import me.bam6561.aethelcomposite.modules.core.references.Module;
 import me.bam6561.aethelcomposite.modules.core.references.Namespaced;
 import me.bam6561.aethelcomposite.modules.core.references.Text;
 import me.bam6561.aethelcomposite.modules.core.references.markers.ItemStackValue;
+import me.bam6561.aethelcomposite.modules.core.references.markers.ModuleRecipeValue;
 import me.bam6561.aethelcomposite.modules.core.references.markers.NamespacedKeyValue;
 import me.bam6561.aethelcomposite.utils.ItemUtils;
 import net.md_5.bungee.api.ChatColor;
@@ -111,7 +112,7 @@ public class Lasso {
    * @version 1.0.89
    * @since 1.0.89
    */
-  public enum Recipe {
+  public enum Recipe implements ModuleRecipeValue {
     /**
      * {@link Item#IRON_LASSO}
      */
