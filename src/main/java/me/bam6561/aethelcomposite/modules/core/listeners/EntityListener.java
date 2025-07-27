2b6e93726749fbd504ad50f23d2dccb0097ae8f2
Danny Nguyen <ndanny09@gmail.com>
2025-07-27 12:46:24 -0400

    1.1.19: Define HookShotEntity & EntityListener onProjectileHitEvent

--- src/main/java/me/bam6561/aethelcomposite/modules/core/listeners/EntityListener.java
+++ src/main/java/me/bam6561/aethelcomposite/modules/core/listeners/EntityListener.java
@@ -1,19 +1,24 @@
 package me.bam6561.aethelcomposite.modules.core.listeners;
 
+import me.bam6561.aethelcomposite.modules.core.objects.entity.ModuleEntity;
 import me.bam6561.aethelcomposite.modules.core.objects.item.ModuleItemStack;
+import me.bam6561.aethelcomposite.modules.core.utils.EntityUtils;
 import me.bam6561.aethelcomposite.modules.core.utils.ItemUtils;
+import me.bam6561.aethelcomposite.modules.hook.objects.HookShotEntity;
 import me.bam6561.aethelcomposite.modules.hook.objects.HookShotItem;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
 
 /**
  * Collection of entity interaction listeners.
  *
  * @author Danny Nguyen
- * @version 1.1.13
+ * @version 1.1.19
  * @since 1.1.10
  */
 public class EntityListener implements Listener {
@@ -44,6 +49,25 @@ public class EntityListener implements Listener {
     ModuleItemStack moduleItem = new ModuleItemStack(consumable);
     switch (moduleItem.getModuleName()) {
       case HOOK -> new HookShotItem(moduleItem.getItem()).modifyProjectile(event);
+    }
+  }
+
+  /**
+   * Routes projectile hit interactions.
+   *
+   * @param event projectile hit event
+   */
+  @EventHandler
+  private void onProjectileHitEvent(ProjectileHitEvent event) {
+    Entity projectile = event.getEntity();
+    String entityID = EntityUtils.Read.getEntityID(projectile);
+    if (entityID == null) {
+      return;
+    }
+
+    ModuleEntity moduleEntity = new ModuleEntity(projectile);
+    switch (moduleEntity.getModuleName()) {
+      case HOOK -> new HookShotEntity(projectile).pullShooter(event);
     }
   }
 }
---
+++ src/main/java/me/bam6561/aethelcomposite/modules/hook/objects/HookShotEntity.java
@@ -0,0 +1,82 @@
+package me.bam6561.aethelcomposite.modules.hook.objects;
+
+import me.bam6561.aethelcomposite.modules.core.objects.entity.ModuleEntity;
+import me.bam6561.aethelcomposite.modules.core.references.Namespaced;
+import me.bam6561.aethelcomposite.modules.core.utils.TextUtils;
+import me.bam6561.aethelcomposite.modules.hook.references.Hook;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * Represents a {@link Hook.SpawnableEntity#HOOK_SHOT}.
+ * <p>
+ * Hook shots are projectiles that pull the shooter towards their point of impact.
+ *
+ * @author Danny Nguyen
+ * @version 1.1.19
+ * @since 1.1.19
+ */
+public class HookShotEntity extends ModuleEntity {
+  /**
+   * Associates the HookShotEntity with its entity.
+   *
+   * @param entity interacting entity
+   */
+  public HookShotEntity(@NotNull Entity entity) {
+    super(Objects.requireNonNull(entity, "Null entity"));
+    if (Hook.SpawnableEntity.HOOK_SHOT != Hook.SpawnableEntity.valueOf(TextUtils.Format.asEnum(getEntityID()))) {
+      throw new IllegalArgumentException();
+    }
+    if (!(entity instanceof Arrow)) {
+      throw new IllegalArgumentException();
+    }
+  }
+
+  /**
+   * Pulls the {@link Hook.SpawnableEntity#HOOK_SHOT} shooter towards its point of impact.
+   *
+   * @param event projectile hit event
+   */
+  public void pullShooter(@NotNull ProjectileHitEvent event) {
+    Objects.requireNonNull(event, "Null event");
+    Arrow hookShot = (Arrow) event.getEntity();
+    hookShot.getPersistentDataContainer().remove(Namespaced.Key.Core.MODULE.asKey());
+    hookShot.getPersistentDataContainer().remove(Namespaced.Key.Entity.ID.asKey());
+
+    if (!(hookShot.getShooter() instanceof LivingEntity shooter)) {
+      return;
+    }
+    if (shooter instanceof Player player) {
+      PlayerInventory pInv = player.getInventory();
+      if (pInv.getItemInMainHand().getType() != Material.CROSSBOW && pInv.getItemInOffHand().getType() != Material.CROSSBOW) {
+        return;
+      }
+    }
+
+    Location impactLoc = event.getEntity().getLocation();
+    Location shooterLoc = shooter.getLocation();
+    if (impactLoc.getWorld() != shooterLoc.getWorld()) {
+      return;
+    }
+
+    Vector impactVector = impactLoc.toVector();
+    Vector shooterVector = shooterLoc.toVector();
+    Vector launchVector = (impactVector.subtract(shooterVector)).normalize();
+
+    double distance = impactLoc.distance(shooterLoc);
+    double amplitude = Math.max(2.5, distance / 4);
+
+    launchVector = new Vector(launchVector.getX() * amplitude, launchVector.getY() * amplitude / 2, launchVector.getZ() * amplitude);
+    shooter.setVelocity(launchVector);
+  }
+}
