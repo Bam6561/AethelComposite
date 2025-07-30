ab85f8d64c11207e43d0116160c55e2ae052b8fd
Danny Nguyen <ndanny09@gmail.com>
2025-07-30 09:17:00 -0400

    1.1.39: Add UniqueModuleItemDamageManager to ModulePlayerListener & Require UniqueModuleItemDamageEvent for UniqueModuleItemDamageManager interpretDamage Method

--- src/main/java/me/bam6561/aethelcomposite/modules/core/listeners/ModulePlayerListener.java
+++ src/main/java/me/bam6561/aethelcomposite/modules/core/listeners/ModulePlayerListener.java
@@ -1,10 +1,12 @@
 package me.bam6561.aethelcomposite.modules.core.listeners;
 
 import me.bam6561.aethelcomposite.Plugin;
+import me.bam6561.aethelcomposite.modules.core.events.UniqueModuleItemDamageEvent;
 import me.bam6561.aethelcomposite.modules.core.events.player.SneakInteractEntityEvent;
 import me.bam6561.aethelcomposite.modules.core.events.player.SneakInteractEvent;
 import me.bam6561.aethelcomposite.modules.core.managers.SneakInteractEntityManager;
 import me.bam6561.aethelcomposite.modules.core.managers.SneakInteractManager;
+import me.bam6561.aethelcomposite.modules.core.managers.UniqueModuleItemDamageManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -18,7 +20,7 @@ import org.bukkit.event.Listener;
  * </ul>
  *
  * @author Danny Nguyen
- * @version 1.1.29
+ * @version 1.1.39
  * @since 1.1.29
  */
 public class ModulePlayerListener implements Listener {
@@ -31,6 +33,11 @@ public class ModulePlayerListener implements Listener {
    * {@link SneakInteractEntityManager}
    */
   private final SneakInteractEntityManager sneakInteractEntityManager = Plugin.getSneakInteractEntityManager();
+
+  /**
+   * {@link UniqueModuleItemDamageManager}
+   */
+  private final UniqueModuleItemDamageManager uniqueModuleItemDamageManager = Plugin.getUniqueModuleItemDamageManager();
 
   /**
    * Routes {@link SneakInteractEvent SneakInteractEvents}.
@@ -56,5 +63,18 @@ public class ModulePlayerListener implements Listener {
       return;
     }
     sneakInteractEntityManager.interpretAction(event.getSource());
+  }
+
+  /**
+   * Routes {@link UniqueModuleItemDamageEvent UniqueModuleItemDamageEvents}.
+   *
+   * @param event {@link UniqueModuleItemDamageEvent}
+   */
+  @EventHandler
+  private void onUniqueModuleItemDamage(UniqueModuleItemDamageEvent event) {
+    if (event.isCancelled()) {
+      return;
+    }
+    uniqueModuleItemDamageManager.interpretDamage(event);
   }
 }
--- src/main/java/me/bam6561/aethelcomposite/modules/core/managers/UniqueModuleItemDamageManager.java
+++ src/main/java/me/bam6561/aethelcomposite/modules/core/managers/UniqueModuleItemDamageManager.java
@@ -1,14 +1,15 @@
 package me.bam6561.aethelcomposite.modules.core.managers;
 
 import me.bam6561.aethelcomposite.modules.core.events.UniqueModuleItemDamageEvent;
-import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 /**
  * Manages {@link UniqueModuleItemDamageEvent} durability changes.
  *
  * @author Danny Nguyen
- * @version 1.1.37
+ * @version 1.1.39
  * @since 1.1.37
  */
 public class UniqueModuleItemDamageManager {
@@ -26,7 +27,7 @@ public class UniqueModuleItemDamageManager {
    *
    * @param event player item damage event
    */
-  public void interpretDamage(@NotNull PlayerItemDamageEvent event) {
-
+  public void interpretDamage(@NotNull UniqueModuleItemDamageEvent event) {
+    Objects.requireNonNull(event, "Null event");
   }
 }
